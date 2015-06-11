896e07839ae0c5252c22c00e493d5491081b13a0
Kabir Roshan <kabir.roshan@hotmail.co.uk>
2015-06-11 21:48:58 +0100

    Testing some stuff...

---
+++ the game.py
@@ -0,0 +1,25 @@
+print ("~~~~~The Game~~~~~")
+name = input("What is your name? ")
+print ("Welcome" , name, "To The Game!")
+print("This Game will test your abilities!")
+print(" ")
+print("Lets BEGIN!")
+answerOne = int(input("What is 2 + 4 * 3? "))
+
+if answerOne == 14:
+    print("Well done! You passed the first question...\n now let the hard questions commence!")
+else:
+      print("WRONG! You have failed the first question...wow...")
+
+answerTwo = int(input("What is 7 * 3? "))
+if answerTwo == 21:
+    print("Correct you have passed the test!")
+else:
+    print("GAME OVER! unlucky", name , "try again next time!")
+
+# noob code.
+# Kabir Roshan.
+        
+
+
+
